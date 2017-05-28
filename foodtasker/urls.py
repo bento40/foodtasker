e88f3a756a879168be9a29aa00a15bd192c3eba2
bento40 <phantaporntr@gmail.com>
2017-05-28 08:26:13 +0700

    270517
    update

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
@@ -9,6 +9,9 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+
+
+    # Restuarant
     url(r'^restaurant/sign-in/$', auth_views.login,
         {'template_name': 'restaurant/sign_in.html'},
         name = 'restaurant-sign-in'),
@@ -17,5 +20,15 @@ urlpatterns = [
         name = 'restaurant-sign-out'),
     url(r'^restaurant/sign-up', views.restaurant_sign_up,
         name = 'restaurant-sign-up'),
-    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home')
+    url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home'),
+
+    url(r'^restaurant/account/$', views.restaurant_account, name = 'restaurant_account'),
+    url(r'^restaurant/meal/$', views.restaurant_meal, name = 'restaurant_meal'),
+    url(r'^restaurant/order/$', views.restaurant_order, name = 'restaurant_order'),
+    url(r'^restaurant/report/$', views.restaurant_report, name = 'restaurant_report'),
+
+    # Sign In/ Sign Up/ Sign Out
+    url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
+    # /covert-token (sign in/ sing up)
+    # /revoke-token (sign out)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)  #add restaurant in admin panel
+admin.site.register(Customer)  #add customer in admin panel
+admin.site.register(Driver)  #add Driver in admin panel
